184aa6057a46e5265880bd5b06d2d18590ca1283
Amanda Muniz <amanda.muniiz@outlook.com>
2021-03-08 16:13:42 -0300

    Creating tests structure and file test
    Co-authored-by: Amanda Muniz <amanda.muniiz@outlook.com>
    Co-authored-by: Leticia Araujo <leticiards.04@gmail.com>

---
+++ src/api/tests/files_tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from rest_framework import status
+from api.models import Project, Story, Intent
+from .views import StoriesFile
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+
+
+class StoriesFileTest(TestCase):
+    def setUp(self):
+        self.story = Story.objects.create(id = '10', name = 'teste', content = [
+            {
+                "name": "content name",
+                "type": "intent"
+            },
+            {
+                "name": "content name 2",
+                "type": "utter"
+            },
+            {
+                "name": "content name 3",
+                "type": "utter"
+            }
+        ])
+        self.factory = APIRequestFactory()
+        self.view = StoriesFile.as_view()
+        self.client = APIClient()
+        self.data = {'id':'10', 'name':'teste', 'content': [
+            {
+                "name": "content name",
+                "type": "intent"
+            },
+            {
+                "name": "content name",
+                "type": "utter"
+            },
+            {
+                "name": "content name",
+                "type": "utter"
+            }
+        ]}
+
+    def test_story_view_success_status_code(self):
+        url = reverse('api:story', kwargs={'pk': self.story.pk})
+        request = self.factory.get(url)
+        response = self.view(request, pk=self.story.pk)
+        self.assertEquals(response.status_code, 200)
